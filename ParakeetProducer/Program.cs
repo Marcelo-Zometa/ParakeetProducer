4cf7e1c82054dfe65f878ee4b90afbe9d5a92caf
Marcelo Zometa Paniagua <marcelo.zometapaniag@ad.snow.edu>
2019-12-11 14:17:56 -0700

    Project runs

--- ParakeetProducer/Program.cs
+++ ParakeetProducer/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Net;
+﻿using Refit;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -11,19 +11,55 @@ namespace ParakeetProducer
         {
             using var client = new HttpClient();
 
+            //Getting book from user
             Console.WriteLine("Please input number for getting the book: ");
 
             var num = Console.ReadLine();
-            var url = $"http://www.gutenberg.org/cache/epub/{num}/pg{num}.txt";
 
+            Console.WriteLine("What is the batch size: ");
+            var batch = Console.ReadLine();
+
+            var url = $"http://www.gutenberg.org/cache/epub/{num}/pg{num}.txt";
+            string APIurl = "http://144.17.24.80:30081";
+
+            //Getting parts of the book.
             var completeBook = await client.GetStringAsync(url);
             var title = completeBook.Substring(0, completeBook.IndexOf('\n'));
+            var partOfBook = completeBook.Substring((title.Length + 1), (title.Length + 1) + 10000);
 
             Console.WriteLine("The book title is " + title);
-
-            completeBook = completeBook.Substring((title.Length + 1), (title.Length + 1) + 10000);
-
             Console.WriteLine("The book's first 10000 characters are: " + completeBook);
+
+            await PostToAPI(APIurl, title, partOfBook);
+        }
+
+
+
+        private static async Task PostToAPI(string APIurl, string title, string book)
+        {
+            var postingService = RestService.For<ITrigramParserAPI>(APIurl);
+
+            try
+            {
+                await postingService.CreateTrigrams(new RequestBody
+                {
+                    Title = title,
+                    Text = book
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error, could not add book");
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        public interface ITrigramParserAPI
+        {
+            [Post("/api/trygram/createtrygrams")]
+            Task CreateTrigrams(RequestBody request);
         }
     }
+
 }
---
+++ ParakeetProducer/RequestBody.cs
@@ -0,0 +1,8 @@
+﻿namespace ParakeetProducer
+{
+    public class RequestBody
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
